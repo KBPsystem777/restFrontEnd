ee466de9725835859bdd3735e95e1664b6c3e148
kbpsystem777 <koleen.bp@outlook.com>
2019-02-21 04:41:30 +0800

    importing FoodItem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import FoodItem from './Components/FoodItem';
 
 const API = 'http://localhost:8080/api/dishes';
 
@@ -25,9 +26,42 @@ class App extends Component {
       console.log(err)
     })
   }
+
+  Style = {
+    margin: '10px',
+    padding: '10px'
+  }
   render() {
     return (
-      <div>My App</div>
+      <div>
+        <div className="ui list" style={this.Style}>
+          <h1> App's Menu </h1>
+          <FoodItem 
+            picture={this.state.picture[0]}
+            name={this.state.name[0]}
+            price={this.state.price[0]}
+          />
+          <FoodItem 
+            picture={this.state.picture[1]}
+            name={this.state.name[1]}
+            price={this.state.price[1]}
+          />
+          <FoodItem 
+            picture={this.state.picture[2]}
+            name={this.state.name[2]}
+            price={this.state.price[2]}
+          />
+          <div className="item">
+            <img className="ui small rounded image" src={this.state.picture[0]} alt="lasagna"/>
+            <div className="content">
+            <h1><a className="header">{this.state.name[0]}</a></h1>
+            <div className="description">
+              <h3> $ {this.state.price[0]} </h3> .
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
     );
   }
 }
